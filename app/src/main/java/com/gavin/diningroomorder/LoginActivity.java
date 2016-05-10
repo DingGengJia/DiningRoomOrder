4023ca0365efb30c6d5feefba4211edda3314087
gavinding <gengjia.ding@gmail.com>
2016-05-10 23:55:45 +0800

    1、增加记住密码功能

--- app/src/main/java/com/gavin/diningroomorder/LoginActivity.java
+++ app/src/main/java/com/gavin/diningroomorder/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.gavin.diningroomorder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -24,18 +27,54 @@ import cz.msebera.android.httpclient.Header;
  */
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, IBusinessDeleage {
 
+    private static final String LOG_TAG = "LoginActivity";
+    private static final String PREFS_NAME = "prefsname";       // 偏好设置名称
+    public static final String USER_NAME = "username"; //用户名标记
+    public static final String USER_PASSWORD = "password"; //用户名标记
+    private static final String DEFAULT_USERNAME = "am30467"; //默认用户名
+
     EditText mUserName;
     EditText mPassWord;
     Button mLogin;
+    CheckBox mSave;
+    private SharedPreferences mSettings = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         mUserName = (EditText) findViewById(R.id.username);
         mPassWord = (EditText) findViewById(R.id.psd);
         mLogin = (Button) findViewById(R.id.login);
         mLogin.setOnClickListener(this);
+        mSave = (CheckBox) findViewById(R.id.checkBox_save);
+
+        mSettings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        if (getUserName() != null) {
+            mUserName.setText(getUserName()); //设置用户名
+        }
+        if (getPassword() != null && getPassword() != "") {
+            mSave.setChecked(true); //勾选记住密码
+            mPassWord.setText(getPassword());
+        }
+    }
+
+    private String getPassword() {
+        return mSettings.getString(USER_PASSWORD, "");
+    }
+
+    // 保存用户名
+    private void saveUserName(String username) {
+        SharedPreferences.Editor editor = mSettings.edit();// 获取编辑器
+        editor.putString(USER_NAME, username);
+        editor.commit(); //保存数据
+        //editor.clear();//清除数据
+    }
+
+    // 获取保存的用户名
+    private String getUserName() {
+        return mSettings.getString(USER_NAME, DEFAULT_USERNAME);
     }
 
     @Override
@@ -49,8 +88,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 // TODO temp to del
 //                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 //                startActivity(intent);
+
+                saveUserName(mUserName.getText().toString());
+
+                if (mSave.isChecked()) {
+                    savePassword(mPassWord.getText().toString());
+                } else {
+                    savePassword("");
+                }
                 break;
         }
+    }
+
+    private void savePassword(String password) {
+        SharedPreferences.Editor editor = mSettings.edit();// 获取编辑器
+        editor.putString(USER_PASSWORD, password);
+        editor.commit(); //保存数据
     }
 
     @Override
--- app/src/main/java/util/PreferenceUtil.java
+++ app/src/main/java/util/PreferenceUtil.java
@@ -10,14 +10,13 @@ import com.gavin.diningroomorder.CustomApplication;
  */
 public class PreferenceUtil {
 
-    public static void setCookie(Context ctx,String cookie)
-    {
+    public static void setCookie(Context ctx, String cookie) {
         Log.d("setSession", "cookies =" + cookie);
-        ((CustomApplication)ctx).setsCookie(cookie);
+        ((CustomApplication) ctx).setsCookie(cookie);
     }
 
-    public static String getCookie(Context ctx)
-    {
-        return ((CustomApplication)ctx).getsCookie();
+    public static String getCookie(Context ctx) {
+        return ((CustomApplication) ctx).getsCookie();
     }
+
 }
